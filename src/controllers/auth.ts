6865227a22b6d59b83bec0885d9300c19a463f26
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-05-16 20:27:10 +0200

    auth controller created

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,11 +2,20 @@ import { Request, Response } from "express";
 import { registerNewUser, loginUser } from '../services/auth';
 
 const loginController = async ({body}:Request, response:Response) => {
-    
+    const { email, password } = body;
+    const responseUser = await loginUser({email, password});
+
+    if (responseUser === "PASSWORD_INCORRECT") {
+        response.status(403);
+        response.send(responseUser);
+    } else {
+        response.send(responseUser);
+    }
 }
 
-const registerController = async (request:Request, response:Response) => {
-
+const registerController = async ({body}:Request, response:Response) => {
+    const responseUser = await registerNewUser(body);
+    response.send(responseUser);
 }
 
 export { loginController, registerController };
